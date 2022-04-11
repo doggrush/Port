52871b160b06bec7381392841318e56d2384d3a8
doggrush <72785829+doggrush@users.noreply.github.com>
2022-04-11 10:44:43 -0300

    Update and rename Main.java to Progressão Aritmética.java

--- Progressão Aritmética.java
+++ Progressão Aritmética.java
@@ -5,13 +5,13 @@ public class Main
 		int a1 = 15;
 		int n = 19;
 		int r = 8;
-		int an = a1 + (n-1)*r; //calcula An
+		int an = a1 + (n-1)*r; 
 		System.out.println("Usando a formula de PA, o valor de an é " + an);
-		a1= an -(n-1)*r; // calcula A1
+		a1= an -(n-1)*r; 
 		System.out.println("Usando usando a formula PA, o valor de a1 é " + a1);
-		r =(an-a1)/(n-1); // calcula N
+		r =(an-a1)/(n-1); 
 		System.out.println("Usando a formula de PA, o valor de r é " + r);
-		n = (an-a1)/r + 1; // calula N
+		n = (an-a1)/r + 1; 
 		System.out.println("Usando a formula de PA, o valor de n é " + n);
 		
 	}
