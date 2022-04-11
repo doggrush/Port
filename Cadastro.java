3cc316a1e1bc4acfcdb24f8e3230ea938878b0c1
doggrush <72785829+doggrush@users.noreply.github.com>
2022-04-11 10:35:07 -0300

    Update and rename Bofao.java to Cadastro.java

--- Cadastro.java
+++ Cadastro.java
@@ -1,17 +1,17 @@
-package bofao;
+package Attividades;
 
 import javax.swing.JOptionPane;
 
-public class Bofao {
+public class Cadastro {
 
     public static void main(String[] args) {
         String nome = JOptionPane.showInputDialog("Digite seu nome");
-        JOptionPane.showMessageDialog(null, "Bem vindo, " + nome);
+        JOptionPane.showMessageDialog(null, "Bem vindo " + nome);
         String idade = JOptionPane.showInputDialog("Digite sua idade");
         JOptionPane.showMessageDialog(null, "Você é " + nome + " e tem " + idade + " anos?");
         String resposta = JOptionPane.showInputDialog("Escreva sim se você é " + nome + " ou digite não se não for " + nome + ".");
         if (resposta.equals("sim")) {
-            JOptionPane.showMessageDialog(null, "Parabens você é "+nome+".");
+            JOptionPane.showMessageDialog(null, "Sua identidade foi validada e foi cadastrada no sistema.");
         } else {
             JOptionPane.showMessageDialog(null, "Infelizmente você não é "+nome+".");
             String nome_real = JOptionPane.showInputDialog(null, "Agora digite seu nome verdadeiro.");
